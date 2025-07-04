636cc75ab126c8b85a4bcd52430da5e61da2167c
Marume0813 <2301204@s.asojuku.ac.jp>
2025-07-04 11:05:45 +0900

    データベース情報追加

--- db.php
+++ db.php
@@ -1,1 +1,15 @@
+<?php
+// ロリポップ用の接続情報
+$dsn = 'mysql:host=mysql325.phy.lolipop.lan;dbname=LAA1553906-todo;charset=utf8mb4';
+$user = 'LAA1553906';
+$password = 'Pass0813'; 
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'DB接続エラー: ' . $e->getMessage();
+    exit;
+}
+?>
 
